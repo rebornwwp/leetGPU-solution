83238c81babf24ba01fadb6fafc342f79040446e
MC_pengzi <rebornwwp@gmail.com>
2025-04-27 12:59:38 +0000

    fix: add

---
+++ Vector_Addition/5.cu
@@ -0,0 +1,42 @@
+#include <cuda_runtime.h>
+
+#include <vector>
+
+#include "solve.h"
+
+#define FLOAT4(value) (reinterpret_cast<float4*>(&(value))[0])
+
+__global__ void vector_add(float* __restrict__ a, float* __restrict__ b, float* c, int N) {
+  int idx = 4 * (blockIdx.x * blockDim.x + threadIdx.x);
+  int stride = 4 * blockDim.x * gridDim.x;
+
+  for (int i = idx; i + 3 < N; i += stride) {
+    float4 a4 = FLOAT4(a[i]);
+    float4 b4 = FLOAT4(b[i]);
+    float4 c4;
+    c4.x = a4.x + b4.x;
+    c4.y = a4.y + b4.y;
+    c4.z = a4.z + b4.z;
+    c4.w = a4.w + b4.w;
+    FLOAT4(c[i]) = c4;
+  }
+
+  // optional: scalar tail处理 N 不被4整除的情况
+  // 让一个wrap来处理tail,  分担负载，加速优化
+  int lane_id = threadIdx.x % 32;
+  int tail_start = (N / 4) * 4;
+  if (blockIdx.x == 0 && lane_id < (N - tail_start)) {
+    int i = tail_start + lane_id;
+    c[i] = a[i] + b[i];
+  }
+}
+
+// A, B, C are device pointers (i.e. pointers to memory on the GPU)
+void solve(const float* A, const float* B, float* C, int N) {
+  // 需要更细致的分配线程
+  int threadsPerBlock = 256;
+  int blocksPerGrid = (N + 4 * threadsPerBlock - 1) / (4 * threadsPerBlock);
+
+  vector_add<<<blocksPerGrid, threadsPerBlock>>>((float*)A, (float*)B, C, N);
+  cudaDeviceSynchronize();
+}
---
+++ Vector_Addition/readme.md
@@ -0,0 +1,4 @@
+# 更多的优化
+
+1. __ldg()只读cache
+2. occupancy 自动调节参数, 动态block/grid数量分配
